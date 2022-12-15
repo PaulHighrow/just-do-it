c47a6d47ea6bef046fa6264e957b27a97b3931a7
VadimKorobec <_s_e_t__@ukr.net>
2022-12-15 15:41:50 +0200

    Create render-gallery.js

---
+++ src/js/render-gallery.js
@@ -0,0 +1,11 @@
+<li class="card">
+  <div class="card_tumb">
+    <img class="card_img" id="" src="" alt="" />
+  </div>
+  <h2 class="card_title"></h2>
+  <div class="card_caption"></div>
+  <ul class="card_list">
+    <li class="card_item"></li>
+  </ul>
+  <span class="card_year"></span>
+</li>;
